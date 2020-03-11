8d06cb3a0ab7852f79a3cf052231c3a2612e016b
Markus Lons <markckv@gmail.com>
2020-03-12 01:20:02 +0700

    Боже надеюсь это работает

--- new_game.py
+++ new_game.py
@@ -16,18 +16,26 @@ class MapController:
 		self.map_history = deque([], maxlen=32)
 		self.size = size
 		self.gen_map()
-
 		self.step_counter = 0
 		self.available_a = 0
 		self.available_b = 0
 
 		self.GamerGay = Gamer(self.size)
 		self.GamerStraight = Gamer(self.size)
+		self.coordinatesGay = (0, 0)
+		self.coordinatesStraight = (0, 0)
 	def viewer(self):
 		plt.imshow(np.reshape(self.map_history[-1], (10, 10)), cmap='hot', interpolation='nearest')
 		plt.show()
 		plt.pause(0.0001)
 		plt.clf()
+	def map_compression(self):
+		CMap = list()
+		for y in range(0,len(self.map_history[-1]), 2):
+			for x in range(0, len(self.map_history[-1]), 2):
+				list.append(self.map_history[-1][x][y])
+		return CMap
+
 	def gen_map(self):
 		map_array = np.ones(self.size * self.size // 2)
 		a, b = 0, 0
@@ -38,7 +46,19 @@ class MapController:
 		self.map_history.append(map_array)
 
 	def make_step(self, who, gender_step):
-		pass
+		newMap = deepcopy(self.map_history[-1])
+		# step gay
+		newMap[self.coordinatesGay[0]][self.coordinatesGay[1]] = -1
+		coordinatesMoving = self.GamerGay.get_step(self.compression_map())
+		self.coordinatesGay = (self.coordinatesGay[0] + coordinatesMoving[0], self.coordinatesGay[1] + coordinatesMoving[1])
+		newMap[self.coordinatesGay[0], self.coordinatesGay[1]] = 11
+
+		# step straight
+		newMap[self.coordinatesStraight[0]][self.coordinatesStraight[1]] = -1
+		coordinatesMoving = self.GamerStraight.get_step(self.compression_map())
+		self.coordinatesStraight = (self.coordinatesStraight[0] + coordinatesMoving[0], self.coordinatesStraight[1] + coordinatesMoving[1])
+		newMap[self.coordinatesStraight[0], self.coordinatesStraight[1]] = 10
+		self.map_history.append(newMap)
 
 	def get_available_step(self, who):
 		a = self.map_history[-1][0]
@@ -53,7 +73,7 @@ class Gamer:
 	def __init__(self, size):
 		self.size = size
 		self.model = self.create_model()
-		self.stepDecode = {1: -5, 2: -4, 3: 4, 4: 3}
+		self.c = {1: -5, 2: -4, 3: 4, 4: 3}
 
 	def predict_step(self, map):
 		pred = self.model.predict(map)
